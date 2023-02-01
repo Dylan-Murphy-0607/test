46cd53239f7fb1961dd84bcfdf40812929ccc12a
Dylan-Murphy-0607 <murpawq@bc.edu>
2023-02-01 15:53:33 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Dylan Murphy on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
